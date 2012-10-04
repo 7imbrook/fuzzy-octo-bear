f4bc40b2105a52e3bfb8316c7c97758a2958d4a2
Michael Timbrook <timbrook480@gmail.com>
2012-10-04 17:09:47 -0400

    Cal formating
    Calendar formated with  start day, and number of days in month.

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,27 @@ exports.index = function(req, res){
 
 exports.gtv = function(req, res){
 
+	var offset = 2;				//Sunday is 1, Monday is 2 etc...
+	var daysinmounth = 31;		//Number of days in the month
+	var arrayD = {};
+	var daycounter = 1;	
+	for(var x = 1; x<=35; x++){
+		if(x < offset || daycounter > daysinmounth){
+			arrayD[x] = '';
+		} else {
+			arrayD[x] = daycounter;
+			daycounter++;
+		}
+	}
+
 	res.render('cal', {
 		title: 'for Google TV',
-		days: 31,
-		offset: 0,
+		month: 10,
+		days: arrayD,
 		events: {
-			'10/4': 'Awesomeness',
-			'10/18': 'Random Day',
-			'10/26': 'Birthday'
+			4 : 'Awesomeness',
+			18 : 'Random Day',
+			26 : 'Birthday'
 		}
 	});
 };
