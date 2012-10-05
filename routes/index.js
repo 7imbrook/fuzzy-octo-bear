580f708da7bb8c0b4686ccd77bd66368c6aebeae
Michael Timbrook <timbrook480@gmail.com>
2012-10-04 21:46:52 -0400

    Display Events
    No suport for dulpicate events

--- routes/index.js
+++ routes/index.js
@@ -24,12 +24,41 @@ exports.gtv = function(req, res){
 
 	res.render('cal', {
 		title: 'for Google TV',
-		month: 10,
 		days: arrayD,
+		month: 10,
 		events: {
-			4 : 'Awesomeness',
-			18 : 'Random Day',
-			26 : 'Birthday'
+			'Event': {
+				dateTime: 'Thu Oct 04 2012 19:30:00 GMT-0400 (EDT)',
+				info: 'Event information'
+			},
+			'Second Event': {
+				dateTime: 'Tue Oct 09 2012 20:00:00 GMT-0400 (EDT)',
+				info: 'More event information'
+			},
+			'Third Event': {
+				dateTime: 'Thu Oct 18 2012 19:00:00 GMT-0400 (EDT)',
+				info: 'INFORMATION'
+			},
+			'Birthday': {
+				dateTime: 'Fri Oct 26 2012 02:45:42 GMT-0400 (EDT)',
+				info: 'Best day ever'
+			},
+			'SSE Meeting1': {
+				dateTime: 'Fri Oct 05 2012 13:00:00 GMT-0400 (EDT)',
+				info: 'Event information'
+			},
+			'SSE Meeting2': {
+				dateTime: 'Fri Oct 12 2012 13:00:00 GMT-0400 (EDT)',
+				info: 'More event information'
+			},
+			'SSE Meeting3': {
+				dateTime: 'Fri Oct 19 2012 13:00:00 GMT-0400 (EDT)',
+				info: 'INFORMATION'
+			},
+			'SSE Meeting4': {
+				dateTime: 'Fri Oct 26 2012 13:00:00 GMT-0400 (EDT)',
+				info: 'Best day ever'
+			}
 		}
 	});
 };
